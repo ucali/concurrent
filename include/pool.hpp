649555ca91dfb5433e021dc8283a60b1abf0836c
Umberto <umberto.califano@gmail.com>
2016-11-06 17:15:20 +0100

    revert

--- include/pool.hpp
+++ include/pool.hpp
@@ -13,298 +13,285 @@
 
 namespace concurrent {
 
-namespace {
+	namespace {
 
-	template <typename R>
-	class _Task {
-	public:
-		typedef std::shared_ptr<_Task<R>> Ptr;
+		template <typename R>
+		class _Task {
+		public:
+			typedef std::shared_ptr<_Task<R>> Ptr;
 
-		_Task(const std::function<R()>& c) : _c(c) { }
+			_Task(const std::function<R()>& c) : _c(c) { }
 
-		virtual void Exec();
+			virtual void Exec();
 
-	protected:
-		const std::function<R()> _c;
+		protected:
+			const std::function<R()> _c;
 
-		_Task(_Task const&) = delete;
-		_Task& operator=(_Task const&) = delete;
-	};
-
-	template<typename R>
-	void _Task<R>::Exec() { _c(); }
-
-	template <typename T>
-	struct _func_traits {
-		typedef std::function<void(T)> FuncType;
-	};
-
-	template <>
-	struct _func_traits<void> {
-		typedef std::function<void()> FuncType;
-	};
-
-
-	template <typename R>
-	class Task : public _Task<R> {
-	public:
-		typedef std::shared_ptr<Task<R>> Ptr;
-
-		Task(const std::function<R()>& c, const typename _func_traits<R>::FuncType& t) : _Task<R>(c), _t(t) {}
-		Task(const std::function<R()>& c) : _Task<R>(c) { _t = [](R) {}; }
-
-		virtual void Exec();
-
-	private:
-		typename _func_traits<R>::FuncType _t;
-	};
-
-	template<>
-	Task<void>::Task(const std::function<void()>& c) : _Task<void>(c) { _t = []() {}; }
-
-	template<>
-	void Task<void>::Exec() { this->_c(); this->_t(); }
-
-	template<typename R>
-	void Task<R>::Exec() { this->_t(std::move(this->_c())); }
-
-}
-
-class WaitGroup {
-public:
-	typedef std::shared_ptr<WaitGroup> Ptr;
-
-	WaitGroup(size_t s) : _s(s) {
-		_c.store(0);
-
-		for (int i = 0; i < s; i++) {
-			_c.fetch_add(1);
-		}
-	}
-
-	void Add() {
-		_c.fetch_add(1);
-	}
-
-	size_t Size() const { return _s; }
-
-	void Finish() {
-		_c.fetch_sub(1);
-		_empty.notify_all();
-	}
-
-
-	void Wait() {
-		std::unique_lock<std::mutex> lock(_mutex);
-		while (_c.load()) {
-			_empty.wait(lock);
-		}
-	}
-
-private:
-	const size_t _s;
-	std::atomic<unsigned short> _c;
-
-
-	mutable std::mutex _mutex;
-	std::condition_variable _empty;
-
-	WaitGroup(WaitGroup const&) = delete;
-	WaitGroup& operator=(WaitGroup const&) = delete;
-
-};
-
-template <typename R = void, typename ...Args>
-class Pool {
-public:
-	typedef std::shared_ptr<Pool<R, Args...>> Ptr;
-
-	explicit Pool(size_t s = std::thread::hardware_concurrency()) { init(s); }
-	explicit Pool(const std::function<R(Args...)>& c, size_t s = std::thread::hardware_concurrency()) : _c(c) { init(s); }
-
-	~Pool() { Close(); }
-
-	bool IsRunning() const { return _guard.load(); }
-	size_t Size() const {
-		std::unique_lock<std::mutex> lock(_mutex);
-		return _threads.size();
-	}
-
-	void OnException(const std::function<void(const std::exception&)>& f) {
-		std::unique_lock<std::mutex> lock(_mutex);
-		_ee = f;
-	}
-
-	void OnInit(const std::function<void()>& f) {
-		std::unique_lock<std::mutex> lock(_mutex);
-		_init = f;
-	}
-
-	void canGrow(bool b) {
-		_grow.store(b);
-	}
-
-	template <typename ..._Args>
-	void Spawn(const std::function<void(_Args...)>& c, _Args... args) {
-		if (IsRunning() == false) {
-			return; //Throw
-		}
-
-		auto func = std::bind(c, args...);
-
-		std::unique_lock<std::mutex> lock(_mutex);
-		_threads.emplace_back(func);
-	}
-
-	void Spawn(const std::function<void()>& c) {
-		if (IsRunning() == false) {
-			return; //Throw
-		}
-
-		std::unique_lock<std::mutex> lock(_mutex);
-		_threads.emplace_back(c);
-	}
-
-	template <typename ..._Args>
-	void Send(const std::function<void(_Args...)>& c, _Args... args) {
-		auto fun = std::bind(c, args...);
-		Task<void>::Ptr ptr(new Task<void>(fun));
-
-		launch(ptr);
-	}
-
-	template <typename ..._Args>
-	void Send(const std::function<R(_Args...)>& c, const typename _func_traits<R>::FuncType& t, _Args... args) {
-		auto fun = std::bind(c, args...);
-		typename Task<R>::Ptr ptr(new Task<R>(fun, t));
-
-		launch(ptr);
-	}
-
-	void Send(const std::function<R()>& c, const typename _func_traits<R>::FuncType& t) {
-		typename Task<R>::Ptr ptr(new Task<R>(c, t));
-		launch(ptr);
-	}
-
-	template <typename ..._Args>
-	void Call(_Args... args) {
-		auto fun = std::bind(_c, args...);
-		typename Task<R>::Ptr ptr(new Task<R>(fun));
-
-		launch(ptr);
-	}
-
-	void Send(const std::function<R()>& c, size_t num = 1) {
-		for (int i = 0; i < num; i++) {
-			typename _Task<R>::Ptr ptr(new _Task<R>(c));
-
-			_counter++;
-			checkAndGrow();
-			_msgQ.Push(ptr);
-		}
-	}
-
-	void Close() {
-		try {
-			std::unique_lock<std::mutex> lock(_mutex);
-
-			if (_threads.empty()) {
-				return;
-			}
-
-			_guard.store(false);
-
-			for (auto i = 0; i < _threads.size(); i++) {
-				_msgQ.Push(nullptr);
-			}
-
-			for (auto& t : _threads) {
-				if (t.joinable()) {
-					t.join();
-				}
-			}
-
-			_threads.clear();
-		}
-		catch (...) {
-			std::cerr << "Error shutting down pool." << std::endl;
-		}
-	}
-
-private:
-	void init(size_t s) noexcept {
-		_guard.store(true);
-		_grow.store(true);
-		_counter.store(0);
-
-		_ee = [](const std::exception& e) { std::cerr << "Error: " << e.what() << std::endl; };
-		_init = []() {};
-
-		//add(std::max(8l, long(s)));
-		add(s);
-	}
-
-	bool isAlmostFull() {
-		std::unique_lock<std::mutex> lock(_mutex);
-		return _threads.size() - _counter <= 2;
-	}
-
-	void launch(typename Task<R>::Ptr ptr) {
-		_counter++;
-		checkAndGrow();
-		_msgQ.Push(ptr);
-
-	}
-
-	void checkAndGrow() {
-		if (_grow.load() && isAlmostFull()) {
-			add(2);
-		}
-	}
-
-	void add(size_t s) {
-		auto func = [this] {
-			{
-				std::unique_lock<std::mutex> lock(_mutex);
-				_init();
-			}
-			while (IsRunning()) {
-				try {
-					auto h = _msgQ.Pop();
-					if (h != nullptr) {
-						h->Exec();
-						_counter--;
-					}
-				}
-				catch (const std::exception& e) {
-					std::unique_lock<std::mutex> lock(_mutex);
-					_ee(e);
-				}
-			}
+			_Task(_Task const&) = delete;
+			_Task& operator=(_Task const&) = delete;
 		};
 
-		for (int i = 0; i < s; i++) {
-			_threads.emplace_back(func);
-		}
+		template<typename R>
+		void _Task<R>::Exec() { _c(); }
+
+		template <typename T>
+		struct _func_traits {
+			typedef std::function<void(T)> FuncType;
+		};
+
+		template <>
+		struct _func_traits<void> {
+			typedef std::function<void()> FuncType;
+		};
+
+
+		template <typename R>
+		class Task : public _Task<R> {
+		public:
+			typedef std::shared_ptr<Task<R>> Ptr;
+
+			Task(const std::function<R()>& c, const typename _func_traits<R>::FuncType& t) : _Task<R>(c), _t(t) {}
+			Task(const std::function<R()>& c) : _Task<R>(c) { _t = [](R) {}; }
+
+			virtual void Exec();
+
+		private:
+			typename _func_traits<R>::FuncType _t;
+		};
+
+		template<>
+		Task<void>::Task(const std::function<void()>& c) : _Task<void>(c) { _t = []() {}; }
+
+		template<>
+		void Task<void>::Exec() { this->_c(); this->_t(); }
+
+		template<typename R>
+		void Task<R>::Exec() { this->_t(std::move(this->_c())); }
+
 	}
 
-	SyncQueue<typename _Task<R>::Ptr> _msgQ;
-	std::vector<std::thread> _threads;
+	class WaitGroup {
+	public:
+		typedef std::shared_ptr<WaitGroup> Ptr;
 
-	std::atomic_bool _guard;
-	std::atomic_bool _grow;
-	std::atomic<int>_counter;
+		WaitGroup(size_t s) : _s(s) {
+			_c.store(0);
 
-	mutable std::mutex _mutex;
+			for (int i = 0; i < s; i++) {
+				_c.fetch_add(1);
+			}
+		}
+
+		void Add() {
+			_c.fetch_add(1);
+		}
+
+		size_t Size() const { return _s; }
+
+		void Finish() {
+			_c.fetch_sub(1);
+			_empty.notify_all();
+		}
 
 
-	Pool(Pool const&) = delete;
-	Pool& operator=(Pool const&) = delete;
+		void Wait() {
+			std::unique_lock<std::mutex> lock(_mutex);
+			while (_c.load()) {
+				_empty.wait(lock);
+			}
+		}
 
-	const std::function<R(Args...)> _c;
-	std::function<void(const std::exception&)> _ee;
-	std::function<void()> _init;
-};
+	private:
+		const size_t _s;
+		std::atomic<unsigned short> _c;
+
+
+		mutable std::mutex _mutex;
+		std::condition_variable _empty;
+
+		WaitGroup(WaitGroup const&) = delete;
+		WaitGroup& operator=(WaitGroup const&) = delete;
+
+	};
+
+	template <typename R = void, typename ...Args>
+	class Pool {
+	public:
+		typedef std::shared_ptr<Pool<R, Args...>> Ptr;
+
+		explicit Pool(size_t s = std::thread::hardware_concurrency()) { init(s); }
+		explicit Pool(const std::function<R(Args...)>& c, size_t s = std::thread::hardware_concurrency()) : _c(c) { init(s); }
+		explicit Pool(const std::function<R(Args...)>& c,
+			const typename _func_traits<R>::FuncType& t,
+			size_t s = std::thread::hardware_concurrency()) : _c(c), _t(t)
+		{
+			init(s);
+		}
+
+		~Pool() { Close(); }
+
+		bool IsRunning() const { return _guard.load(); }
+		size_t Size() const {
+			std::unique_lock<std::mutex> lock(_mutex);
+			return _threads.size();
+		}
+
+		void OnException(const std::function<void(const std::exception&)>& f) {
+			std::unique_lock<std::mutex> lock(_mutex);
+			_ee = f;
+		}
+
+		template <typename ..._Args>
+		void Spawn(const std::function<void(_Args...)>& c, _Args... args) {
+			if (IsRunning() == false) {
+				return; //Throw
+			}
+
+			auto func = std::bind(c, args...);
+
+			std::unique_lock<std::mutex> lock(_mutex);
+			_threads.emplace_back(func);
+		}
+
+		void Spawn(const std::function<void()>& c) {
+			if (IsRunning() == false) {
+				return; //Throw
+			}
+
+			std::unique_lock<std::mutex> lock(_mutex);
+			_threads.emplace_back(c);
+		}
+
+		template <typename ..._Args>
+		void Send(const std::function<void(_Args...)>& c, _Args... args) {
+			auto fun = std::bind(c, args...);
+			Task<void>::Ptr ptr(new Task<void>(fun));
+
+			launch(ptr);
+		}
+
+		template <typename ..._Args>
+		void Send(const std::function<R(_Args...)>& c, const typename _func_traits<R>::FuncType& t, _Args... args) {
+			auto fun = std::bind(c, args...);
+			typename Task<R>::Ptr ptr(new Task<R>(fun, t));
+
+			launch(ptr);
+		}
+
+		void Send(const std::function<R()>& c, const typename _func_traits<R>::FuncType& t) {
+			typename Task<R>::Ptr ptr(new Task<R>(c, t));
+			launch(ptr);
+		}
+
+		template <typename ..._Args>
+		void Call(_Args... args) {
+			auto fun = std::bind(_c, args...);
+			typename Task<R>::Ptr ptr(new Task<R>(fun));
+
+			launch(ptr);
+		}
+
+		void Send(const std::function<R()>& c, size_t num = 1) {
+			for (int i = 0; i < num; i++) {
+				typename _Task<R>::Ptr ptr(new _Task<R>(c));
+
+				_counter++;
+				if (isAlmostFull()) {
+					add(2);
+				}
+				_msgQ.Push(ptr);
+			}
+		}
+
+		void Close() {
+			try {
+				std::unique_lock<std::mutex> lock(_mutex);
+
+				if (_threads.empty()) {
+					return;
+				}
+
+				_guard.store(false);
+
+				for (auto i = 0; i < _threads.size(); i++) {
+					_msgQ.Push(nullptr);
+				}
+
+				for (auto& t : _threads) {
+					if (t.joinable()) {
+						t.join();
+					}
+				}
+
+				_threads.clear();
+			}
+			catch (...) {
+				std::cerr << "Error shutting down pool." << std::endl;
+			}
+		}
+
+	private:
+		void init(size_t s) noexcept {
+			_guard.store(true);
+			_counter.store(0);
+
+			add(std::max(8l, long(s)));
+
+			_ee = [](const std::exception& e) { std::cerr << "Error: " << e.what() << std::endl; };
+		}
+
+		bool isAlmostFull() {
+			std::unique_lock<std::mutex> lock(_mutex);
+			return _threads.size() - _counter <= 2;
+		}
+
+		void launch(typename Task<R>::Ptr ptr) {
+			_counter++;
+			if (isAlmostFull()) {
+				add(2);
+			}
+			_msgQ.Push(ptr);
+
+		}
+
+		void add(size_t s) {
+			auto func = [this] {
+				while (IsRunning()) {
+					try {
+						auto h = _msgQ.Pop();
+						if (h != nullptr) {
+							h->Exec();
+							_counter--;
+						}
+					}
+					catch (const std::exception& e) {
+						std::unique_lock<std::mutex> lock(_mutex);
+						_ee(e);
+					}
+				}
+			};
+
+			for (int i = 0; i < s; i++) {
+				_threads.emplace_back(func);
+			}
+		}
+
+		SyncQueue<std::shared_ptr<_Task<R>>> _msgQ;
+		std::vector<std::thread> _threads;
+
+		std::atomic_bool _guard;
+		std::atomic<int>_counter;
+
+		mutable std::mutex _mutex;
+
+
+		Pool(Pool const&) = delete;
+		Pool& operator=(Pool const&) = delete;
+
+		const std::function<R(Args...)> _c;
+		const typename _func_traits<R>::FuncType _t;
+		std::function<void(const std::exception&)> _ee;
+	};
 
 }
 
--- include/queue.hpp
+++ include/queue.hpp
@@ -9,251 +9,255 @@
 
 namespace concurrent {
 
-namespace ex {
+	namespace ex {
 
-	class ClosedQueueException : public std::runtime_error {
+		class ClosedQueueException : public std::runtime_error {
+		public:
+			ClosedQueueException(std::string s) : std::runtime_error(s) {}
+		};
+
+		class TimeoutQueueException : public std::runtime_error {
+		public:
+			TimeoutQueueException(std::string s) : std::runtime_error(s) {}
+		};
+
+		class EmptyQueueException : public std::runtime_error {
+		public:
+			EmptyQueueException(std::string s) : std::runtime_error(s) {}
+		};
+
+	}
+
+	template <typename T>
+	class SyncQueue {
 	public:
-		ClosedQueueException(std::string s) : std::runtime_error(s) {}
+		typedef std::shared_ptr<SyncQueue> Ptr;
+
+		typedef size_t KeyType;
+		typedef T ValueType;
+		typedef T Type;
+
+		SyncQueue(size_t t = 1 << 16) : _maxSize(t), _closed(false) { }
+		~SyncQueue() { }
+
+		T Pop();
+		T Pop(uint64_t ms);
+		T PopNoThrow(uint64_t ms);
+
+		void Push(const T&);
+		bool Push(const T&, uint64_t ms);
+
+		void Push(T&&);
+		bool Push(T&&, uint64_t ms);
+
+		void WakeAndClose();
+
+		inline bool IsEmpty() const { std::unique_lock<std::mutex> lock(_mutex); return _queue.size() == 0; }
+		inline bool IsFull() const { std::unique_lock<std::mutex> lock(_mutex); return _queue.size() == _maxSize; }
+		inline size_t Size() const { std::unique_lock<std::mutex> lock(_mutex); return _queue.size(); }
+
+		inline void Close() {
+			{
+				std::unique_lock<std::mutex> lock(_mutex);
+				_closed = true;
+			}
+			_empty.notify_all();
+			_full.notify_all();
+		}
+
+		void WaitForEmpty() {
+			std::unique_lock<std::mutex> lock(_mutex);
+			while (!_closed || _queue.size()) {
+				_full.wait(lock);
+			}
+		}
+
+		void Wait() {
+			std::unique_lock<std::mutex> lock(_mutex);
+			while (!_closed) {
+				_full.wait(lock);
+			}
+		}
+
+		inline bool IsClosed() const { std::unique_lock<std::mutex> lock(_mutex); return _closed; }
+		inline bool IsOpen() const { std::unique_lock<std::mutex> lock(_mutex); return !_closed; }
+		inline bool CanReceive() const {
+			std::unique_lock<std::mutex> lock(_mutex);
+			return !_closed || _queue.size();
+		}
+
+		void ForEach(const std::function<void(const Type&)>& fn) {
+			while (CanReceive()) {
+				fn(Pop());
+			}
+		}
+
+		template <typename Storage>
+		void Aggregate(const std::function<void(const KeyType&, Storage&&)>& fn) {
+			Storage storage;
+			while (CanReceive()) {
+				storage.push_back(std::move(Pop()));
+			}
+			fn(0, storage);
+		}
+
+		void Clear() {
+			//std::unique_lock<std::mutex> lock(_mutex);
+		}
+
+	protected:
+		T& First() { return _queue.front(); }
+		const T& First() const { return _queue.front(); }
+
+		T& Last() { return _queue.back(); }
+		const T& Last() const { return _queue.back(); }
+
+	private:
+		std::queue<T> _queue;
+		const size_t _maxSize;
+
+		bool _closed;
+
+		mutable std::mutex _mutex;
+		std::condition_variable _empty;
+		std::condition_variable _full;
+
+		SyncQueue(SyncQueue const&) = delete;
+		SyncQueue& operator=(SyncQueue const&) = delete;
 	};
 
-	class TimeoutQueueException : public std::runtime_error {
-	public:
-		TimeoutQueueException(std::string s) : std::runtime_error(s) {}
-	};
+	template <typename T>
+	T SyncQueue<T>::Pop() {
+		T t;
 
-	class EmptyQueueException : public std::runtime_error {
-	public:
-		EmptyQueueException(std::string s) : std::runtime_error(s) {}
-	};
-
-}
-
-template <typename T>
-class SyncQueue {
-public:
-	typedef std::shared_ptr<SyncQueue> Ptr;
-
-	typedef size_t KeyType;
-	typedef T ValueType;
-	typedef T Type;
-
-	SyncQueue(size_t t = 1 << 16) : _maxSize(t), _closed(false) { }
-	~SyncQueue() { }
-
-	T Pop();
-	T Pop(uint64_t ms);
-	T PopNoThrow(uint64_t ms);
-
-	void Push(const T&);
-	bool Push(const T&, uint64_t ms);
-
-	void Push(T&&);
-	bool Push(T&&, uint64_t ms);
-
-	void WakeAndClose();
-
-	inline bool IsEmpty() const { std::unique_lock<std::mutex> lock(_mutex); return _queue.size() == 0; }
-	inline bool IsFull() const { std::unique_lock<std::mutex> lock(_mutex); return _queue.size() == _maxSize; }
-	inline size_t Size() const { std::unique_lock<std::mutex> lock(_mutex); return _queue.size(); }
-
-	inline void Close() {
 		{
 			std::unique_lock<std::mutex> lock(_mutex);
-			_closed = true;
-		}
-		_empty.notify_all();
-		_full.notify_all();
-	}
-
-	void WaitForEmpty() {
-		std::unique_lock<std::mutex> lock(_mutex);
-		_full.wait(lock, [this] { return _closed && _queue.size() == 0; });
-	}
-
-	void Wait() {
-		std::unique_lock<std::mutex> lock(_mutex);
-		_full.wait(lock, [this] { return _closed; });
-	}
-
-	inline bool IsClosed() const { std::unique_lock<std::mutex> lock(_mutex); return _closed; }
-	inline bool IsOpen() const { std::unique_lock<std::mutex> lock(_mutex); return !_closed; }
-	inline bool CanReceive() const {
-		std::unique_lock<std::mutex> lock(_mutex);
-		return !_closed || _queue.size();
-	}
-
-	void ForEach(const std::function<void(const Type&)>& fn) {
-		while (CanReceive()) {
-			fn(Pop());
-		}
-	}
-
-	template <typename Storage>
-	void Aggregate(const std::function<void(const KeyType&, Storage&&)>& fn) {
-		Storage storage;
-		while (CanReceive()) {
-			storage.push_back(std::move(Pop()));
-		}
-		fn(0, storage);
-	}
-
-	void Clear() {
-		//std::unique_lock<std::mutex> lock(_mutex);
-	}
-
-protected:
-	T& First() { return _queue.front(); }
-	const T& First() const { return _queue.front(); }
-
-	T& Last() { return _queue.back(); }
-	const T& Last() const { return _queue.back(); }
-
-private:
-	std::queue<T> _queue;
-	const size_t _maxSize;
-
-	bool _closed;
-
-	mutable std::mutex _mutex;
-	std::condition_variable _empty;
-	std::condition_variable _full;
-
-	SyncQueue(SyncQueue const&) = delete;
-	SyncQueue& operator=(SyncQueue const&) = delete;
-};
-
-template <typename T>
-T SyncQueue<T>::Pop() {
-	T t;
-
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		while (_queue.size() == 0) {
-			if (_closed) {
-				_full.notify_all();
-				throw ex::ClosedQueueException("Pop: closed queue");
-			}
-
-			_empty.wait(lock);
-		}
-
-		t = _queue.front();
-		_queue.pop();
-	}
-
-	_full.notify_all();
-	return std::move(t);
-}
-
-template <typename T>
-T SyncQueue<T>::Pop(uint64_t ms) {
-	T t;
-
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		if (_queue.size() == 0) {
-			if (_empty.wait_for(lock, std::chrono::milliseconds(ms)) == std::cv_status::timeout) {
+			while (_queue.size() == 0) {
 				if (_closed) {
+					_full.notify_all();
 					throw ex::ClosedQueueException("Pop: closed queue");
 				}
-				throw ex::TimeoutQueueException("Pop: timeout");
+
+				_empty.wait(lock);
 			}
 
+			t = _queue.front();
+			_queue.pop();
+		}
+
+		_full.notify_all();
+		return std::move(t);
+	}
+
+	template <typename T>
+	T SyncQueue<T>::Pop(uint64_t ms) {
+		T t;
+
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
 			if (_queue.size() == 0) {
-				throw ex::EmptyQueueException("Pop: empty");
-			}
-		}
+				if (_empty.wait_for(lock, std::chrono::milliseconds(ms)) == std::cv_status::timeout) {
+					if (_closed) {
+						throw ex::ClosedQueueException("Pop: closed queue");
+					}
+					throw ex::TimeoutQueueException("Pop: timeout");
+				}
 
-		t = _queue.front();
-		_queue.pop();
-	}
-
-	_full.notify_all();
-	return std::move(t);
-}
-
-template <typename T>
-T SyncQueue<T>::PopNoThrow(uint64_t ms) {
-	try {
-		return std::move(Pop(ms));
-	}
-	catch (...) {
-		return T();
-	}
-}
-
-
-template <typename T>
-void SyncQueue<T>::WakeAndClose() {
-	std::unique_lock<std::mutex> lock(_mutex);
-	if (_closed) { return; }
-
-	_empty.notify_all();
-	_queue.push(T());
-	_closed = true;
-}
-
-template <typename T>
-void SyncQueue<T>::Push(const T& p) {
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		while (_queue.size() == _maxSize) {
-			_full.wait(lock);
-		}
-
-		_queue.push(std::move(p));
-	}
-	_empty.notify_all();
-}
-
-template <typename T>
-bool SyncQueue<T>::Push(const T& p, uint64_t ms) {
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		if (_queue.size() == _maxSize) {
-			if (_full.wait_for(lock, std::chrono::milliseconds(ms)) == std::cv_status::timeout) {
-				return false;
-			}
-		}
-
-		_queue.push(std::move(p));
-	}
-	_empty.notify_all();
-	return true;
-}
-
-template <typename T>
-void SyncQueue<T>::Push(T&& p) {
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		while (_queue.size() == _maxSize) {
-			_full.wait(lock);
-		}
-
-		_queue.push(std::move(p));
-	}
-	_empty.notify_all();
-}
-
-template <typename T>
-bool SyncQueue<T>::Push(T&& p, uint64_t ms) {
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		if (_queue.size() == _maxSize) {
-			if (_full.wait_for(lock, std::chrono::milliseconds(ms)) == std::cv_status::timeout) {
-				return false;
+				if (_queue.size() == 0) {
+					throw ex::EmptyQueueException("Pop: empty");
+				}
 			}
 
+			t = _queue.front();
+			_queue.pop();
+		}
+
+		_full.notify_all();
+		return std::move(t);
+	}
+
+	template <typename T>
+	T SyncQueue<T>::PopNoThrow(uint64_t ms) {
+		try {
+			return std::move(Pop(ms));
+		}
+		catch (...) {
+			return T();
+		}
+	}
+
+
+	template <typename T>
+	void SyncQueue<T>::WakeAndClose() {
+		std::unique_lock<std::mutex> lock(_mutex);
+		if (_closed) { return; }
+
+		_empty.notify_all();
+		_queue.push(T());
+		_closed = true;
+	}
+
+	template <typename T>
+	void SyncQueue<T>::Push(const T& p) {
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
+			while (_queue.size() == _maxSize) {
+				_full.wait(lock);
+			}
+
+			_queue.push(std::move(p));
+		}
+		_empty.notify_all();
+	}
+
+	template <typename T>
+	bool SyncQueue<T>::Push(const T& p, uint64_t ms) {
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
 			if (_queue.size() == _maxSize) {
-				return false;
+				if (_full.wait_for(lock, std::chrono::milliseconds(ms)) == std::cv_status::timeout) {
+					return false;
+				}
 			}
-		}
 
-		_queue.push(std::move(p));
+			_queue.push(std::move(p));
+		}
+		_empty.notify_all();
+		return true;
 	}
-	_empty.notify_all();
-	return true;
-}
+
+	template <typename T>
+	void SyncQueue<T>::Push(T&& p) {
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
+			while (_queue.size() == _maxSize) {
+				_full.wait(lock);
+			}
+
+			_queue.push(std::move(p));
+		}
+		_empty.notify_all();
+	}
+
+	template <typename T>
+	bool SyncQueue<T>::Push(T&& p, uint64_t ms) {
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
+			if (_queue.size() == _maxSize) {
+				if (_full.wait_for(lock, std::chrono::milliseconds(ms)) == std::cv_status::timeout) {
+					return false;
+				}
+
+				if (_queue.size() == _maxSize) {
+					return false;
+				}
+			}
+
+			_queue.push(std::move(p));
+		}
+		_empty.notify_all();
+		return true;
+	}
 
 }
 
