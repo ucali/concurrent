02c72366b025b69ed867108fc5a01387b6a58195
Umberto <umberto.califano@gmail.com>
2016-12-02 10:19:19 +0100

    ifdef

--- include/fiber.hpp
+++ include/fiber.hpp
@@ -41,7 +41,7 @@ public:
 			_cnd.notify_all();
 		}));
 			
-	#ifdef __linux__
+	#ifndef WIN32
 		boost::fibers::use_scheduling_algorithm<boost::fibers::algo::shared_work>(); 
 	#endif
 		boost::fibers::fiber([ptr] {
