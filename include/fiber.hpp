e5b6c39658e6bbfcac7d4eed836e440ded554a83
Umberto <umberto.califano@gmail.com>
2016-12-02 10:47:31 +0100

    Revert "ifdef"
    This reverts commit d0f26cbb9bf872c9f4a4f8fbfd4b182c1ecaaae1.

--- include/fiber.hpp
+++ include/fiber.hpp
@@ -41,7 +41,9 @@ public:
 			_cnd.notify_all();
 		}));
 			
+	#ifndef WIN32
 		boost::fibers::use_scheduling_algorithm<boost::fibers::algo::shared_work>(); 
+	#endif
 		boost::fibers::fiber([ptr] {
 			ptr->Exec();
 		}).detach();
